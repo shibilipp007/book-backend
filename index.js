05e0939404aff7d200aeba29e95adb1eaf3d7769
shibilipp007 <shibilishareef007@gmail.com>
2024-08-02 20:26:55 +0530

    second change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
